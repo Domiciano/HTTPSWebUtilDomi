e1f72906e616218ed3c82ac3ae17f4e6b405f757
Domiciano <domiciano.rincon@correounivalle.edu.co>
2021-10-30 12:31:24 -0500

    Update HTTPSWebUtilDomi.java

--- HTTPSWebUtilDomi.java
+++ HTTPSWebUtilDomi.java
@@ -25,40 +25,6 @@ import javax.net.ssl.X509TrustManager;
 
 public class HTTPSWebUtilDomi {
 
-    public HTTPSWebUtilDomi() {
-        try {
-            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
-                public X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-
-                public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                }
-
-                public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                }
-            }
-            };
-            //Install the all-trusting trust manager
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
-            // Install the all-trusting host verifier
-            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession sslSession) {
-                    return true;
-                    //Use la variable hostname para retornar true en caso de que
-                    //concuerde con la página que usted está intentando consultar
-                }
-            });
-            //HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-        } catch (NoSuchAlgorithmException | KeyManagementException e) {
-            e.printStackTrace();
-        }
-    }
-
     public String GETrequest(String url) {
         try {
             URL page = new URL(url);
@@ -85,8 +51,47 @@ public class HTTPSWebUtilDomi {
             URL page = new URL(url);
             HttpsURLConnection connection = (HttpsURLConnection) page.openConnection();
             connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json-patch+json");
+            connection.setRequestProperty("Content-Type", "application/json");
             connection.setRequestProperty("accept", "application/json");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+
+            OutputStream os = connection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+
+            writer.write(json);
+            writer.flush();
+
+            InputStream is = connection.getInputStream();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            baos.close();
+            os.close();
+            connection.disconnect();
+            return new String(baos.toByteArray(), "UTF-8");
+        }catch (IOException ex){
+            ex.printStackTrace();
+            return "";
+        }
+    }
+
+
+    public static final String FCM_KEY = "AAAA5Y28dsA:APA91bHvrWHBdEZIbpl2hmPufyiNgJp1ZNiCenoAb58dM2ydWkGTx5cYtQzlVewJUL2PL0s1Rkir0mTLKtEA0vrrHyeG1bID3HuCd0UVtO91bcuciPPWxZcSsosmRGmGV9N4tEeH90Gr";
+    
+    public String POSTtoFCM(String json) {
+        try {
+            URL page = new URL("https://fcm.googleapis.com/fcm/send");
+            HttpsURLConnection connection = (HttpsURLConnection) page.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("accept", "application/json");
+            connection.setRequestProperty("Authorization", "key="+FCM_KEY);
             connection.setDoInput(true);
             connection.setDoOutput(true);
 
